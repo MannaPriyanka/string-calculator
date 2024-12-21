6ac1a8768ceb4752e8be9c6be4b09094ec6f7c17
PriyankaManna <mannapriyanka04@gmail.com>
2024-12-21 12:12:27 +0530

    Handle single number input and non-numeric input in the add method

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -7,5 +7,15 @@ RSpec.describe StringCalculator do
       calculator = StringCalculator.new
       expect(calculator.add("")).to eq(0)
     end
+
+    it 'returns the number itself for single numeric input' do
+      calculator = StringCalculator.new
+      expect(calculator.add("1")).to eq(1)
+    end
+
+    it 'returns 0 for a non-numeric input' do
+      calculator = StringCalculator.new
+      expect(calculator.add("abc")).to eq(0)
+    end
   end
 end
--- string_calculator.rb
+++ string_calculator.rb
@@ -2,7 +2,8 @@
 class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
-    
+    return numbers.to_i if numbers.match(/^\d+$/)
+
     0
   end
 end
